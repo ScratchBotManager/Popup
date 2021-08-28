9e1d9915d7f547412b2b755a44c2d821da591245
HackitHatchet <84136976+ScratchBotManager@users.noreply.github.com>
2021-08-28 05:54:03 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
 function Popup(title, text, color) {
-document.write( '<div id=\"popupbox\" style=\"background-color: grey; border: 1px solid green; width: 200px; height: 150px;  margin:0 auto;   text-align: center;\">\n' );
-document.write( '<p id=\"popuptitle\">hi</p>\n' );
-document.write( '<hr>\n' );
-document.write( '<p id=\"popuptext\">Hello World</p>\n' );
-document.write( '<button onclick=\"el = document.getElementById(\'popupbox\'); el.remove()\">Close</button>\n' );
+document.write( '<div id="popupbox" style="background-color: grey; border: 1px solid green; width: 200px; height: 150px;  margin:0 auto;   text-align: center;">' );
+document.write( '<p id="popuptitle">hi</p>' );
+document.write( '<hr>' );
+document.write( '<p id="popuptext">Hello World</p>' );
+document.write( '<button onclick="el = document.getElementById("popupbox"); el.remove()\">Close</button>\n' );
 document.write( '</div>' );
 popuptitle = document.getElementById("popuptitle");
 popuptext = document.getElementById("popuptext");
